cdaed9658d04afb360c8764f82cd3b9e191cef30
Afrida-Islam <afrida0627@gmail.com>
2025-12-18 23:17:20 +0600

    update and fic error

--- index.js
+++ index.js
@@ -26,11 +26,23 @@ try {
 
 const app = express();
 
-const allowedOrigins = ["https://assignment011-dkra.vercel.app"];
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "https://assignment011-dkra.vercel.app",
+];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
